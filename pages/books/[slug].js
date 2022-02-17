bdcce31d5609cd408296eb14690ecef0ac1d9de5
Adam.Carter <adamkristopher@gmail.com>
2022-02-17 14:20:31 -0600

    start wordpressifying the app

--- pages/books/[slug].js
+++ pages/books/[slug].js
@@ -22,7 +22,7 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Post({ postData }) {
+export default function Book({ postData }) {
   return (
     <Layout>
       <Head>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import fetcher from '../lib/fetcher'
+import { JEFF_POSTS } from '../lib/wordpress/api'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  // Add the "await" keyword like this:
+  const response = await fetcher(JEFF_POSTS)
+  const jeffPosts = response.data.posts.nodes
   return {
     props: {
-      allPostsData
-    }
+      jeffPosts
+    },
+    revalidate: 1,
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ jeffPosts }) {
   return (
     <Layout home>
       <Head>
@@ -26,14 +30,14 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {jeffPosts.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-               {date}
+                <Date dateString={date} />
               </small>
             </li>
           ))}
